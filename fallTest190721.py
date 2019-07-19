fcb051b0aa51caa80130feddd953afff59119084
unknown <hp7316062@gmail.com>
2019-07-19 15:09:55 +0900

    Update fallTest190721.py
    modify releaseandland

--- fallTest190721.py
+++ fallTest190721.py
@@ -1,50 +1,44 @@
+	# -*- coding: utf-8 -*-
 import sys
+sys.path.append('/home/pi/git/kimuralab/SensorModuleTest/GPS')
+sys.path.append('/home/pi/git/kimuralab/SensorModuleTest/IM920')
 sys.path.append('/home/pi/git/kimuralab/SensorModuleTest/BME280')
 sys.path.append('/home/pi/git/kimuralab/SensorModuleTest/BMX055')
 sys.path.append('/home/pi/git/kimuralab/SensorModuleTest/Camera')
-sys.path.append('/home/pi/git/kimuralab/SensorModuleTest/GPS')
-sys.path.append('/home/pi/git/kimuralab/SensorModuleTest/IM920')
-sys.path.append('/home/pi/git/kimuralab/SensorModuleTest/Melting')
-sys.path.append('/home/pi/git/kimuralab/SensorModuleTest/Motor')
 sys.path.append('/home/pi/git/kimuralab/SensorModuleTest/TSL2561')
-sys.path.append('/home/pi/git/kimuralab/IntegratedProgram/ParaAvoidance')
 import time
-import serial
+import difflib
 import pigpio
-import BME280
-import BMX055
+import serial
+import binascii
 import IM920
 import GPS
-import Melting
-import Motor
+import BMX055
+import BME280
+import Capture
 import TSL2561
-import ParaAvoidance
-
+import Release
+import Land
 luxstr = ["lux1", "lux2"]																#variable to show lux returned variables
 bme280str = ["temp", "pres", "hum", "alt"]												#variable to show bme280 returned variables
 bmx055str = ["accx", "accy", "accz", "gyrx", "gyry", "gyrz", "dirx", "diry", "dirz"]	#variable to show bmx055 returned variables
 gpsstr = ["utctime", "lat", "lon", "sHeight", "gHeight"]								#variable to show GPS returned variables
-
+PRESS=[0.0,0.0]
 t_setup = 10	#variable to set waiting time after setup
 
 t = 1	#waitingtime
 x = 61	#time for release(loopx)
 y = 60	#time for land(loopy)
 z = 120	#time for run(loopz)
-lcount=0
+#lcount=0
 acount=0
 Pcount=0
 GAcount=0
 luxmax=300
-deltAmax=0.3
-deltPmax=0.05
 deltHmax=5
-
-
-pi = pigpio.pi()   #object of pigpio
+pi=pigpio.pi()
 def gpsSend(gpsData):
 	IM920.Send('g'+str(gpsData[0]) + ',' + str(gpsData[1]) + ',' + str(gpsData[2]) + ',' + str(gpsData[3]) + ',' + str(gpsData[4]) + ',' + str(gpsData[5]))
-
 def setup():
 	pi.set_mode(22,pigpio.OUTPUT)
 	pi.write(22,0)#IM920
@@ -52,7 +46,7 @@ def setup():
 	time.sleep(1)
 	BME280.bme280_setup()
 	BME280.bme280_calib_param()
-	BMX055.bmx055_setup()
+#	BMX055.bmx055_setup()
 	GPS.openGPS()
 	with open('log/releaseLog.txt', 'a') as f:
 		pass
@@ -65,178 +59,57 @@ def close():
 	GPS.closeGPS()
 	#it may be necessarry to down low status some gpio pins 
 
-
 if __name__ == "__main__":
 	try:
-    	# ------------------- Setup Fhase ------------------- #
+		# ------------------- Setup Fhase ------------------- --#
 		print("Program Start  {0}".format(time.time()))
 		setup()
 		time.sleep(t_setup)
-		bme280Data=BME280.bme280_read()
+	
 
 
 		tx1 = time.time()
 		tx2 = tx1
-
 		# ------------------- Release Fhase ------------------- #
 		print("Releasing Judgement Program Start  {0}".format(time.time()))
 		#loopx
-		while(1):
-
-			#tx2update
-			tx2=time.time()
-			#release judgement(lux)
-			luxdata=TSL2561.readLux()
-			#f.write(str(luxdata[0])+":"+str(luxdata[1]))
-			print("lux1: "+str(luxdata[0])+" "+"lux2: "+str(luxdata[1]))
-			with open('log/releaseLog.txt', 'a') as f:
-				f.write(str(luxdata[0])+"	"+str(luxdata[1])+"		")
-
-			print(lcount)
-			print(acount)
-
-			if luxdata[0]>luxmax or luxdata[1]>luxmax:
-				lcount+=1
-			elif luxdata[0]<luxmax and luxdata[1]<luxmax:
-				lcount=0
-			if lcount>4:
-				luxreleasejudge=True
-				print("luxreleasejudge")
-			else:
-				luxreleasejudge=False
-			#releasejudgement(press)
-				PRESS=bme280Data[1]
-				deltA=PRESS
-				bme280Data=BME280.bme280_read()	#update
-				PRESS=bme280Data[1]
-				deltA=PRESS-deltA
-				#f.write("P0:P1"+str(P0)+":"+str(P1))
-				print(str(PRESS))
-				print(str(deltA))
-				time.sleep(2)
-				#compare with 3 second ago
-				#acout+=1
-				if deltA>deltAmax:
-					acount+=1
-				elif deltA<deltAmax:
-					acount=0
-				if acount>2:
-					presreleasejudge=True
-					print("presjudge")
-
-				else:
-					presreleasejudge=False
-			with open('log/releaseLog.txt', 'a') as f:
-				f.write("\n")
-				f.write(str(deltA)+"   "+str(PRESS)+"\t")
-				f.write("\n")
-
-			if luxreleasejudge or presreleasejudge:
-				ty1=time.time()
-				ty2=ty1
-				print("RELEASE!")
-				with open('log/releaseLog.txt', 'a') as f:
-					f.write("release")
-				pi.write(22,0)
-				bme280Data=BME280.bme280_read()
-				gpsData = GPS.readGPS()
-			# ------------------- Landing Fhase ------------------- #
-				while(ty2-ty1<=y):
-					print(str(ty2-ty1))
-					ty2=time.time()
-					#presjudgement
-					PRESS=bme280Data[1]
-					deltP=PRESS
-					bme280Data=BME280.bme280_read()	#update
-					PRESS=bme280Data[1]
-					deltP=deltP-PRESS
-					with open('log/landingLog.txt', 'a') as f2:
-						f2.write("\n")
-						f2.write(str(deltP))#+":"+str(PRESS)+ "\t")
-						f2.write("\n")
-					print(deltP)
-					print("Pcount:"+str(Pcount))
-					if abs(deltP)<deltPmax:
-						Pcount+=1
-					elif abs(deltP)>deltPmax:
-						Pcount=0
-					if Pcount>4:
-						preslandjudge=True
-						print("preslandjudge")
-					else:
-						preslandjudge=False
-
-					#GPS alt judgement
-					Gheight=gpsData[4]
-					deltH=Gheight
-					gpsData=GPS.readGPS()
-					Gheight=gpsData[4]
-					deltH=deltH-Gheight
-					print(Gheight)
-					#judge every 4sec
-					time.sleep(3)
-					if abs(deltH)<deltHmax:
-						GAcount+=1
-					elif abs(deltH)>deltHmax :
-						GAcount=0
-					if GAcount>4:
-						GPSlandjudge=True
-						print("GPSlandjudge")
-					else:
-						GPSlandjudge=False
-
-
-					#take photo when chenging pres and GPSalt
-					if not preslandjudge and not GPSlandjudge:
-						print("satueinow")
-					#taking photo
-					# out loop y when no change pres and GPSalt
-					elif preslandjudge and  GPSlandjudge:
-						break
-					else:
-						print("Landingjudgement now")
-						
-				else:
-					print("THE ROVER HAS LANDED(timeout y).  {0}".format(time.time()))
-					with open('log/landingLog.txt', 'a') as f2:
-						f2.write("land")
-					break
-				print("THE ROVER HAS LANDED.  {0}".format(time.time()))
-				with open('log/landingLog.txt', 'a') as f2:
-					f2.write("land")
+		bme280Data=BME280.bme280_read()
+		PRESS[0]=bme280Data[1]
+		while (tx2-tx1<=x):
+			luxjudge=Release.luxjudge()
+			pressjudge=Release.pressjudge()
+			if luxjudge==1 or pressjudge==1:
 				break
-			#enter loop x
-			elif tx2-tx1>=x:
-				tz1=time.time()
-				tz2=tz1
-				print("X timeout")
-				#loopx
-				while(tz2-tz1<=z):
-					tz2=time.time()
-					PRESS=bme280Data[1]
-					deltP=PRESS
-					bme280Data=BME280.bme280_read()	#bme update
-					PRESS=bme280Data[1]
-					deltP=deltP-PRESS
-					print(PRESS)
-					#judge every 4sec.
-					time.sleep(3)
-					if abs(deltP)<deltPmax:
-						Pcount+=1
-					elif abs(deltP)>deltPmax:
-						Pcount=0
-					if Pcount>5:
-						preslandjudge=True
-						print("preslandjudge")
-					else:
-						preslandjudge=False
-					#out loopz when not presjudge
-					if  preslandjudge:
-						break
-				#continue if not break in loopz 
-				else:
-					break
-				break					
+			else:
+		   		print("now in rocket ,taking photo")
+			time.sleep(2)
+			tx2=time.time()
+		else:
+			print("RELEASE TIMEOUT")
+		print("THE ROVER HAS RELEASED")
+		pi.write(22,1)
+		# ------------------- Landing Fhase ------------------- #
+		print("Releasing Judgement Program Start  {0}".format(time.time()))
+		ty1=time.time()
+		ty2=ty1
+		gpsData = GPS.readGPS()
+		bme280Data=BME280.bme280_read()
+		PRESS[0]=bme280Data[1]
+		while(ty2-ty1<=y):
+			pressjudge=Land.pressjudge()
+			gpsjudge=Land.gpsjudge()
+			if pressjudge ==1 and gpsjudge ==1:
+			    break
+			elif pressjudge==0 and gpsjudge==0:
+			    print("Descend now taking photo")
+			elif pressjudge==1 or gpsjudge==1:
+			    print("landjudgementnow")
+			time.sleep(3)
+			ty2=time.time()
+		else:
+			print("RELEASE TIMEOUT")
+		print("THE ROVER HAS LANDED")
+				
 		# ------------------- Melting Fhase ------------------- #
 		Melting.Melting()
 
